c75a7cf18c7e3f4a0f57a6e61ddac803dab8da2b
victoria <v.suprun1961@gmail.com>
2024-07-24 00:20:46 +0300

    fix: add button and date

--- test.js
+++ test.js
@@ -2,12 +2,15 @@ console.log("Hello world!");
 
 document.querySelector("#btn").onclick = function() {
     let output = document.querySelector("#output");
-    output.innerHTML = "Hello, my name is Victoriia!";
+    let name = "Виктория!"
+    output.innerHTML = "Привет," + " " + name ;
     output.style.color = "red";
-    output.style.fontSize = 36 + 'px';
+    output.style.fontSize = 30 + 'px';
 }
 
 document.querySelector("#btn_1").onclick = function() {
     let date = document.querySelector("#date");
     date.innerHTML = "Сейчас" + " " + new Date();
+    date.style.color = "red";
+    date.style.fontSize = 30 + 'px';
 }
